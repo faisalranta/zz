35c7ac232d93b528a7b704900ffa99c8d8695139
Shah Faisal Ranta <faisalranta090@gmail.com>
2018-04-10 12:06:50 +0500

    Testing One

--- app/Http/routes/web.php
+++ app/Http/routes/web.php
@@ -94,7 +94,6 @@ Route::group(['prefix' => 'ccd','before' => 'csrf'], function () {
 });
 
 //End Companies
-
 //Start Users
 Route::group(['prefix' => 'users','before' => 'csrf'], function () {
 	Route::get('/u', 'UsersController@toDayActivity');
